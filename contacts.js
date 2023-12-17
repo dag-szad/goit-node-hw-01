8d5251317ef4863981dcfe916f53656bd5f3732d
Dag <dagmara.szadkowska@wp.pl>
2023-12-17 11:20:39 +0100

    Initial setup

---
+++ contacts.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = reqiure('path');
+
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+    // ...twój kod
+  }
+  
+  function getContactById(contactId) {
+    // ...twój kod
+  }
+  
+  function removeContact(contactId) {
+    // ...twój kod
+  }
+  
+  function addContact(name, email, phone) {
+    // ...twój kod
+  }
